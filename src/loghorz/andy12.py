3ff849349c05df7d408bd5bb47a7e2b38d2dc50e
Sydney Pasloski <sydney.pasloski@ucalgary.ca>
2025-05-07 11:16:04 -0600

    fixed colouring and graphs for highres

--- src/loghorz/andy12.py
+++ src/loghorz/andy12.py
@@ -7,13 +7,51 @@ las = lasio.read("../../las_files/HighResolution_full_SelCrv.LAS")
 
 df = las.df()
 df['DEPTH'] = df.index
-print(df.describe())
+print(f'df describe: {df.describe()}')
 
-df.plot(x='GR', y='DEPTH', color='black', linewidth=0.5, legend=False, figsize=(4,8))
-plt.fill_betweenx(df['DEPTH'], df['GR'], 75, facecolor='yellow')
-plt.fill_betweenx(df['DEPTH'], df['GR'], 0, facecolor='white')
-plt.axvline(75, color='black', linewidth=0.5, alpha=0.5)
-plt.title('Filled Gamma Curve')
-plt.savefig('../../figures/filled_gamma_curve.png')
+curve_list = las.curves
+print(f'curve list: {curve_list}')
+
+curve_name_dict = {curve.mnemonic: curve.descr for curve in las.curves}
+print(f'dictionary: {curve_name_dict}')
+
+curve_unit_list = []
+for curve in curve_list:
+    curve_unit_list.append(curve.unit)
+print(f'units: {curve_unit_list}')
+
+non_depth_curves = [
+    curve_name.mnemonic for curve_name in curve_list if curve_name.mnemonic != 'DEPT'
+        and curve_name.mnemonic in df.columns]
+
+columns = len(non_depth_curves)
+
+fig, axes = plt.subplots(1, columns, figsize=(columns * 1.5, 8))
+
+for i, curve in enumerate(non_depth_curves):
+    ax = axes[i]
+    if i != 0:  # remove depth y labels except for first plot
+        ax.set_yticklabels([])
+        ax.set_yticks([])
+    unit = curve_unit_list[i + 1]
+
+    if curve != 'GR' and curve != 'PE':
+        df.plot(
+            x=curve, y='DEPTH', color='black', title=curve, ax=ax, xlabel=unit,
+            linewidth=0.5, marker='o', markersize='0.2', alpha=0.5, legend=False)
+    elif curve == 'GR':
+        df.plot(
+            x='GR', y='DEPTH', color='black', title=curve, ax=ax, xlabel=unit,
+            linewidth = 0.5, marker = 'o', markersize = '0.2', alpha=0.5, legend=False)
+        ax.fill_betweenx(df['DEPTH'], df['GR'], 210, facecolor='lightgreen')
+        ax.fill_betweenx(df['DEPTH'], df['GR'], 75, facecolor='yellow')
+        ax.fill_betweenx(df['DEPTH'], df['GR'], 0, facecolor='white')
+        ax.axvline(75, color='black', linewidth=0.5, alpha=0.5)
+    elif curve == 'PE':
+        df.plot(
+            x='PE', y='DEPTH', color='black', title=curve, ax=ax, xlabel=unit,
+            linewidth=0.5, marker='o', markersize='0.2', alpha=0.5, legend=False)
+
+plt.tight_layout()
+plt.savefig('../../figures/filled_highres.png')
 plt.show()
-
