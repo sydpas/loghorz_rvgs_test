ec39085b1e954cb0e128acefcd784778a0e6cdce
Sydney Pasloski <sydney.pasloski@ucalgary.ca>
2025-05-07 09:25:40 -0600

    following andy's code

---
+++ src/loghorz/andy12.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import lasio
+import numpy as np
+
+las = lasio.read("../../las_files/HighResolution_full_SelCrv.LAS")
+
+df = las.df()
+df['DEPTH'] = df.index
+print(df.describe())
+
+df.plot(x='GR', y='DEPTH', color='black', linewidth=0.5, legend=False, figsize=(4,8))
+plt.fill_betweenx(df['DEPTH'], df['GR'], 75, facecolor='yellow')
+plt.fill_betweenx(df['DEPTH'], df['GR'], 0, facecolor='white')
+plt.axvline(75, color='black', linewidth=0.5, alpha=0.5)
+plt.title('Filled Gamma Curve')
+plt.show()
+
