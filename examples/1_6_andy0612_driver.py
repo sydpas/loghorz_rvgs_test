7290dd8b85d50ec2b76d330b3b294a037cea67db
Sydney Pasloski <sydney.pasloski@ucalgary.ca>
2025-05-09 11:49:50 -0600

    aesthetic things

--- examples/1_6_andy0612_driver.py
+++ examples/1_6_andy0612_driver.py
@@ -23,7 +23,7 @@ def main():
     ax1 = ax_list[0]
     col1 = col_list[0]
 
-    fig, ax = plt.subplots( figsize=(col1 * 1.2, 12))
+    fig, ax = plt.subplots(figsize=(col1 * 1.2, 14))
 
     top = well_tops_list[0]
     for horz, depth in top.items():
@@ -31,7 +31,7 @@ def main():
             y = float(depth)
             ax.axhline(y=y, color='red', lw=2, ls='-')
 
-    # creating a new axis but for same graph
+    # creating a new x-axis on the top for next curve but for same graph
     ax2 = ax.twiny()
 
     for curve in ax1:
@@ -39,22 +39,26 @@ def main():
         if curve == 'GR':
             df.plot(
                 x='GR', y='DEPTH', color='black', ax=ax,
-                linewidth=0.5, marker='o', markersize=0.2, alpha=0.5, legend=False)
+                linewidth=0.5, marker='o', markersize=0.2, alpha=0.5, label='GR')
             ax.fill_betweenx(df['DEPTH'], df['GR'], 75, facecolor='yellow')
             ax.fill_betweenx(df['DEPTH'], df['GR'], 0, facecolor='white')
             ax.axvline(75, color='black', linewidth=0.5, alpha=0.5)
-            ax.set_xlabel('GR')
         else:
             df.plot(
-                x=curve, y='DEPTH', color='blue', ax=ax2,
-                linewidth=0.5, marker='o', markersize=0.2, alpha=0.5, legend=False)
-            ax2.set_xlabel(curve)
+                x=curve, y='DEPTH', color='blue', ax=ax2, label=curve,
+                linewidth=0.5, marker='o', markersize=0.2, alpha=0.5)
 
+    # adjusting proper y limits
     ax.set_ylim(df['DEPTH'].min(), df['DEPTH'].max())
-
     # x axis limits
     ax.set_xlim(df[ax1[0]].min(), df[ax1[0]].max())
     ax2.set_xlim(df[ax1[1]].min(), df[ax1[1]].max())
+
+    # combining the legends and putting bottom left
+    lines_1, labels_1 = ax.get_legend_handles_labels()
+    lines_2, labels_2 = ax2.get_legend_handles_labels()
+    ax.legend(lines_1 + lines_2, labels_1 + labels_2, loc='lower left')
+    ax2.get_legend().remove() if ax2.get_legend() else None
 
     ax.grid(True, linestyle='-', alpha=0.3, linewidth=0.5)
     ax.set_title(f'{ax1[0]} and {ax1[1]}', fontweight='bold', fontsize=14)
