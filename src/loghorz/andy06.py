1d1641889e0ac85f6698f9d01f29d22bf3525f83
Sydney Pasloski <sydney.pasloski@ucalgary.ca>
2025-05-09 09:07:36 -0600

    plotted tops as red axhlines with text

--- src/loghorz/andy06.py
+++ src/loghorz/andy06.py
@@ -9,14 +9,18 @@ well_tops = pd.read_csv('../../csv_files/1506tops.csv')
 print(well_tops.head())
 
 well_tops_list = []
-for tops in well_tops:
-    well_tops_list.append(well_tops)
+for _, row in well_tops.iterrows():  # iterrows is pandas
+    tops = {column: row[column] for column in well_tops.columns
+            if pd.notna(row[column]) and column != 'UWI'}  # notna skips NaN
+    well_tops_list.append(tops)
 
 print(f'well tops list: {well_tops_list}')
 
 columns, non_depth_curves, curve_unit_list, df = andy12()  # call bg function
 
-fig, axes = plt.subplots(1, columns, figsize=(columns * 1.3, 12))
+print(f'length of non depth: {len(non_depth_curves)}')
+
+fig, axes = plt.subplots(1, columns, figsize=(columns * 1.2, 12))
 
 for i, curve in enumerate(non_depth_curves):
     ax = axes[i]
@@ -24,14 +28,24 @@ for i, curve in enumerate(non_depth_curves):
         ax.set_yticklabels([])
         ax.set_yticks([])
     unit = curve_unit_list[i + 1]
-    top = well_tops[i + 1]
+    top = well_tops_list[0]
+
+    # plotting tops
+    for horz, depth in top.items():
+        if pd.notna(depth):
+            y = float(depth)
+            ax.axhline(y=y, color='red', lw=2, ls='-')
+            # if i == 0:
+            #     ax.text(x=20, y=y, s=horz, color='red', fontsize=10, ha='center', va='center')
 
     if curve != 'GR' and curve != 'PE':
+        # plotting curves
         df.plot(
             x=curve, y='DEPTH', color='black', title=curve, ax=ax, xlabel=unit,
             linewidth=0.5, marker='o', markersize='0.2', alpha=0.5, legend=False)
         ax.set_title(curve, fontweight='bold', fontsize=14)
-        ax.axhline(top, color='black', linewidth=0.5, alpha=0.5)
+        ax.set_ylim(df['DEPTH'].min(), df['DEPTH'].max())  # ensures the curves have the right limits
+
     elif curve == 'GR':
         df.plot(
             x='GR', y='DEPTH', color='black', title=curve, ax=ax, xlabel=unit,
@@ -40,11 +54,14 @@ for i, curve in enumerate(non_depth_curves):
         ax.fill_betweenx(df['DEPTH'], df['GR'], 0, facecolor='white')
         ax.axvline(75, color='black', linewidth=0.5, alpha=0.5)
         ax.set_title(curve, fontweight='bold', fontsize=14)
+        ax.set_ylim(df['DEPTH'].min(), df['DEPTH'].max())
+
     elif curve == 'PE':
         df.plot(
             x='PE', y='DEPTH', color='black', title=curve, ax=ax, xlabel=unit,
             linewidth=0.5, marker='o', markersize='0.2', alpha=0.5, legend=False)
         ax.set_title(curve, fontweight='bold', fontsize=14)
+        ax.set_ylim(df['DEPTH'].min(), df['DEPTH'].max())
 
 plt.tight_layout()
 plt.savefig('../../figures/tops_highres.png')
