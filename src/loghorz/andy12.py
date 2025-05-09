70da81c684c8dac1599290a55a0ca2e3b4da69e1
Sydney Pasloski <sydney.pasloski@ucalgary.ca>
2025-05-09 09:19:13 -0600

    took away plotting and combined into driver

--- src/loghorz/andy12.py
+++ src/loghorz/andy12.py
@@ -1,4 +1,3 @@
-import matplotlib.pyplot as plt
 import lasio
 
 
@@ -39,46 +38,3 @@ def andy12():
     columns = len(non_depth_curves)
 
     return columns, non_depth_curves, curve_unit_list, df
-
-
-def main():
-    """
-    This function takes the andy12 and andy06 functions and plots curves.
-    """
-
-    columns, non_depth_curves, curve_unit_list, df = andy12()
-
-    fig, axes = plt.subplots(1, columns, figsize=(columns * 1.2, 12))
-
-    for i, curve in enumerate(non_depth_curves):
-        ax = axes[i]
-        if i != 0:  # remove depth y labels except for first plot
-            ax.set_yticklabels([])
-            ax.set_yticks([])
-        unit = curve_unit_list[i + 1]
-
-        if curve != 'GR' and curve != 'PE':
-            df.plot(
-                x=curve, y='DEPTH', color='black', title=curve, ax=ax, xlabel=unit,
-                linewidth=0.5, marker='o', markersize='0.2', alpha=0.5, legend=False)
-            ax.set_title(curve, fontweight='bold', fontsize=14)
-        elif curve == 'GR':
-            df.plot(
-                x='GR', y='DEPTH', color='black', title=curve, ax=ax, xlabel=unit,
-                linewidth=0.5, marker='o', markersize='0.2', alpha=0.5, legend=False)
-            ax.fill_betweenx(df['DEPTH'], df['GR'], 75, facecolor='yellow')
-            ax.fill_betweenx(df['DEPTH'], df['GR'], 0, facecolor='white')
-            ax.axvline(75, color='black', linewidth=0.5, alpha=0.5)
-            ax.set_title(curve, fontweight='bold', fontsize=14)
-        elif curve == 'PE':
-            df.plot(
-                x='PE', y='DEPTH', color='black', title=curve, ax=ax, xlabel=unit,
-                linewidth=0.5, marker='o', markersize='0.2', alpha=0.5, legend=False)
-            ax.set_title(curve, fontweight='bold', fontsize=14)
-
-    plt.tight_layout()
-    plt.savefig('../../figures/filled_highres.png')
-    plt.show()
-
-if __name__ == '__main__':
-    main()
